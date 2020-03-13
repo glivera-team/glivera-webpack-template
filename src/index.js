f0c51e98df4f2d1c4d2c69fceedaffcac44e5f75
Gavrilov Nikolay <n.gavrilov83@gmail.com>
2020-03-13 13:47:53 +0200

    add template

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// JS
+import './js/';
+
+// SCSS
+import './assets/scss/main_global.scss';
+
+// CSS (example)
+// import './assets/css/main.css'
+
+
+// Vue.js
+// window.Vue = require('vue');
+
+// Vue components (for use in html)
+// Vue.component('example-component', require('./vue_components/Example.vue').default);
+
+// Vue init
+// const app = new Vue({
+//   el: '#app'
+// });
